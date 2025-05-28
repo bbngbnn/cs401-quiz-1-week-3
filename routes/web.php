900fd78c2cbe37b2d8e434539c5dbb62413a959c
dyorgie <tejjlagmay@gmail.com>
2025-05-28 14:37:17 +0800

    set up routes

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,7 @@ Route::get('/get-csrf-token', function () {
 });
 Route::get('/', [GamesController::class,])->name('.index');
 // Step 2. make the routes for the rest of the actions
+
+Route::get('/games', [GamesController::class, 'index'])->name('games.index');
+Route::get('/games/{id}', [GamesController::class, 'show'])->name(name: 'games.show');
+Route::delete('/games/{id}', [GamesController::class, 'delete'])->name('games.destroy');
